34e629390194d08c131063ebcaea7221b330f418
Valeriia Moskalenko <moskalenkovvy@gmail.com>
2024-06-25 15:48:07 +0300

    base urls and pages added

--- stepproject/weather/views.py
+++ stepproject/weather/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, "main.html")
+
+
+def weather(request, city):
+    return render(request, "weather-by-city.html", { 'city':city })
