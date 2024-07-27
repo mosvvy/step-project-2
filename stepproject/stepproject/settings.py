a93c8b75d69ab611229b6ba959c8a661d8c2e1fc
Valeriia Moskalenko <moskalenkovvy@gmail.com>
2024-07-27 17:23:34 +0300

    registration and authorization added

--- stepproject/stepproject/settings.py
+++ stepproject/stepproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'weather',
+    'register'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ STATIC_ROOT = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
